56ac395e76552d46725d7b56d806285a18384dd9
paragon0107 <smg0725@naver.com>
2024-10-07 16:06:35 +0900

    seminar1

---
+++ src/main/java/org/example/practice/seminar1/DiaryRepository.java
@@ -0,0 +1,50 @@
+package org.example.practice.seminar1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class DiaryRepository {
+    private final Map<Long, String> storage = new ConcurrentHashMap<>();
+    private final AtomicLong numbering = new AtomicLong();
+
+    public void save(Diary diary) {
+        final long id = numbering.addAndGet(1);
+        storage.put(id, diary.getBody());
+    }
+
+    public void update(long id, Diary diary) {
+        storage.put(id, diary.getBody());
+    }
+
+    public List<Diary> findAll() {
+        List<Diary> diaries = new ArrayList<>();
+        for (long index = 1; index <= numbering.longValue(); index++) {
+            final String body = storage.get(index);
+            diaries.add(Diary.of(index, body));
+        }
+        return diaries;
+    }
+
+    public void updateDiary(long id, String body) {
+        searchDiaryById(id);
+        update(id, Diary.of(id, body));
+    }
+
+    public void deleteDiaryById(long id) {
+        searchDiaryById(id);
+        for (long i = id; i < numbering.get(); i++) {
+            storage.put(i, storage.get(i + 1));
+        }
+        storage.remove(numbering.getAndDecrement());
+    }
+
+    private void searchDiaryById(long id) {
+        if (!storage.containsKey(id)) {
+            throw new NoSuchElementException("아이디가 존재하지 않습니다");
+        }
+    }
+}
---
+++ src/main/java/org/example/practice/seminar1/DiaryService.java
@@ -0,0 +1,37 @@
+package org.example.practice.seminar1;
+
+import java.util.List;
+
+public class DiaryService {
+    private final DiaryRepository diaryRepository = new DiaryRepository();
+
+    public void createDiary(final String body) {
+        try {
+            Diary diary = Diary.of(null, body);
+            diaryRepository.save(diary);
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+
+    public List<Diary> getDiaryList() {
+        return diaryRepository.findAll();
+    }
+
+    public void updateDiary(final long id, final String body) {
+        try {
+            diaryRepository.updateDiary(id, body);
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
+
+    }
+
+    public void deleteDiary(final long id) {
+        try {
+            diaryRepository.deleteDiaryById(id);
+        }catch (Exception exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+}
