c33745ea12a68eddbba79fe224c4d211f89e57ba
paragon0107 <smg0725@naver.com>
2024-10-11 13:41:49 +0900

    글자수 카운팅 메서드 이모지 인식할 수 있도록 수정

--- src/main/java/org/example/practice/seminar1/Diary.java
+++ src/main/java/org/example/practice/seminar1/Diary.java
@@ -1,5 +1,7 @@
 package org.example.practice.seminar1;
 
+import com.ibm.icu.text.BreakIterator;
+
 public class Diary {
     private Long id;
     private final String body;
@@ -8,8 +10,9 @@ public class Diary {
         this.id = id;
         this.body = body;
     }
-    public static Diary of(Long id,String body){
-        if(!checkBodyLength(body)){
+
+    public static Diary of(Long id, String body) {
+        if (!checkBodyLength(body)) {
             throw new IllegalArgumentException("일기는 30자를 넘길 수 없습니다.");
         }
         return new Diary(id, body);
@@ -23,7 +26,13 @@ public class Diary {
         return body;
     }
 
-    private static boolean checkBodyLength(String body){
-        return body.length() <= 30;
+    private static boolean checkBodyLength(String body) {
+        BreakIterator it = BreakIterator.getCharacterInstance();
+        it.setText(body);
+        int count = 0;
+        while (it.next() != BreakIterator.DONE) {
+            count++;
+        }
+        return count <= 30;
     }
 }
