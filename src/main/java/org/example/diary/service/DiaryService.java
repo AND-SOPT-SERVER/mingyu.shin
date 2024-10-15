cad19f0134398c7b226dc50fff759606d5e9e590
paragon0107 <smg0725@naver.com>
2024-10-15 19:10:23 +0900

    CRUD 로직 추가

--- src/main/java/org/example/diary/service/DiaryService.java
+++ src/main/java/org/example/diary/service/DiaryService.java
@@ -1,5 +1,6 @@
 package org.example.diary.service;
 
+import org.example.diary.api.DiaryUpdateRequest;
 import org.example.diary.repository.DiaryEntity;
 import org.example.diary.repository.DiaryRepository;
 import org.springframework.stereotype.Component;
@@ -7,6 +8,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class DiaryService {
@@ -22,7 +24,7 @@ public class DiaryService {
     }
 
     public List<Diary> getList() {
-        final List<DiaryEntity> diaryEntityList = diaryRepository.findTop10ByOrderByDateAsc();
+        final List<DiaryEntity> diaryEntityList = diaryRepository.findTop10ByCreatedDate();
         final List<Diary> diaryList = new ArrayList<>();
         for (DiaryEntity diaryEntity : diaryEntityList) {
             diaryList.add(new Diary(diaryEntity.getId(), diaryEntity.getTitle()));
@@ -30,7 +32,26 @@ public class DiaryService {
         return diaryList;
     }
 
-    public Diary getDiarySpecific(long id){
+    public DiaryEntity getDiarySpecific(final long id){
+        return getDiaryEntity(id);
+    }
 
+    public void updateDiary(final long id,final DiaryUpdateRequest request){
+        DiaryEntity diaryEntity = getDiaryEntity(id);
+        diaryEntity.setBody(request.body());
+        diaryRepository.save(diaryEntity);
+    }
+
+    public void deleteDiary(final long id){
+        DiaryEntity diaryEntity = getDiaryEntity(id);
+        diaryRepository.delete(diaryEntity);
+    }
+
+    private DiaryEntity getDiaryEntity(final long id) {
+        Optional<DiaryEntity> diaryEntity = diaryRepository.findById(id);
+        if(diaryEntity.isEmpty()){
+            throw new RuntimeException("존재하지 않은 아이디");
+        }
+        return diaryEntity.get();
     }
 }
