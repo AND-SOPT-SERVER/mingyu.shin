2279ead50e7ce96f9744977297f643cbe771227a
paragon0107 <smg0725@naver.com>
2024-10-17 23:46:53 +0900

    카테고리 조회 로직 추가

--- src/main/java/org/example/diary/service/DiaryService.java
+++ src/main/java/org/example/diary/service/DiaryService.java
@@ -1,6 +1,9 @@
 package org.example.diary.service;
 
+import org.example.diary.api.DiaryCreateRequest;
+import org.example.diary.api.DiarySpecificResponse;
 import org.example.diary.api.DiaryUpdateRequest;
+import org.example.diary.repository.Category;
 import org.example.diary.repository.DiaryEntity;
 import org.example.diary.repository.DiaryRepository;
 import org.springframework.stereotype.Component;
@@ -19,12 +22,33 @@ public class DiaryService {
         this.diaryRepository = diaryRepository;
     }
 
-    public void createDiary(final String title,final String body) {
-        diaryRepository.save(DiaryEntity.of(title,body, LocalDateTime.now()));
+    public void createDiary(final DiaryCreateRequest diaryCreateRequest) {
+        Optional<DiaryEntity> diaryEntity = diaryRepository.findByTitle(diaryCreateRequest.title());
+        if (diaryEntity.isPresent()) {
+            throw new RuntimeException("중복되는 제목입니다.");
+        }
+        diaryRepository.save(
+                DiaryEntity.of(
+                        diaryCreateRequest.title(),
+                        diaryCreateRequest.body(),
+                        diaryCreateRequest.category(),
+                        LocalDateTime.now()
+                ));
     }
 
-    public List<Diary> getList() {
-        final List<DiaryEntity> diaryEntityList = diaryRepository.findTop10ByCreatedDate();
+    public DiarySpecificResponse getDiarySpecific(final long id) {
+        DiaryEntity diaryEntity = getDiaryEntity(id);
+        return new DiarySpecificResponse(
+                diaryEntity.getId(),
+                diaryEntity.getTitle(),
+                diaryEntity.getBody(),
+                diaryEntity.getCategory(),
+                diaryEntity.getDate()
+        );
+    }
+
+    public List<Diary> getListById() {
+        final List<DiaryEntity> diaryEntityList = diaryRepository.findAllOrderByBodyLengthDesc();
         final List<Diary> diaryList = new ArrayList<>();
         for (DiaryEntity diaryEntity : diaryEntityList) {
             diaryList.add(new Diary(diaryEntity.getId(), diaryEntity.getTitle()));
@@ -32,24 +56,24 @@ public class DiaryService {
         return diaryList;
     }
 
-    public DiaryEntity getDiarySpecific(final long id){
-        return getDiaryEntity(id);
+    public List<DiaryEntity> getListByCategory(final Category category){
+        return diaryRepository.findAllByCategory(category);
     }
 
-    public void updateDiary(final long id,final DiaryUpdateRequest request){
+    public void updateDiary(final long id, final DiaryUpdateRequest request) {
         DiaryEntity diaryEntity = getDiaryEntity(id);
         diaryEntity.setBody(request.body());
         diaryRepository.save(diaryEntity);
     }
 
-    public void deleteDiary(final long id){
+    public void deleteDiary(final long id) {
         DiaryEntity diaryEntity = getDiaryEntity(id);
         diaryRepository.delete(diaryEntity);
     }
 
     private DiaryEntity getDiaryEntity(final long id) {
         Optional<DiaryEntity> diaryEntity = diaryRepository.findById(id);
-        if(diaryEntity.isEmpty()){
+        if (diaryEntity.isEmpty()) {
             throw new RuntimeException("존재하지 않은 아이디");
         }
         return diaryEntity.get();
