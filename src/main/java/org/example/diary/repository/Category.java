2d5376f3d36f4255cd5ff29cbb87c0372481f9f0
paragon0107 <smg0725@naver.com>
2024-10-17 23:44:06 +0900

    Catrgory추가

---
+++ src/main/java/org/example/diary/repository/Category.java
@@ -0,0 +1,11 @@
+package org.example.diary.repository;
+
+public enum Category {
+    FOOD,
+    EXERCISE,
+    TRAVEL,
+    FESTIVAL,
+    PARTY,
+    DAILY,
+    ETC;
+}
--- src/main/java/org/example/diary/repository/DiaryEntity.java
+++ src/main/java/org/example/diary/repository/DiaryEntity.java
@@ -14,6 +14,9 @@ public class DiaryEntity {
     @Column
     public String title;
 
+    @Column
+    public Category category;
+
     @Column String body;
     @CreatedDate
     LocalDateTime date;
@@ -21,15 +24,16 @@ public class DiaryEntity {
     protected DiaryEntity(){
     }
 
-    private DiaryEntity(final String title, final String body,final LocalDateTime date){
+    private DiaryEntity(final String title, final String body,final LocalDateTime date,final Category category){
         this.title = title;
         this.body = body;
         this.date = date;
+        this.category = category;
     }
 
-    public static DiaryEntity of(final String title,final String body,final LocalDateTime date){
+    public static DiaryEntity of(final String title, final String body, final Category category, final LocalDateTime date){
         validBodyLength(body);
-        return new DiaryEntity(title, body,date);
+        return new DiaryEntity(title, body,date,category);
     }
 
     public long getId() {
@@ -44,6 +48,10 @@ public class DiaryEntity {
         return body;
     }
 
+    public Category getCategory() {
+        return category;
+    }
+
     public LocalDateTime getDate() {
         return date;
     }
@@ -55,7 +63,7 @@ public class DiaryEntity {
 
     private static void validBodyLength(final String body){
         if(body.length() > 30){
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException("일기의 글자수는 30자 이하입니다."); //413 Content Too Large, 400
         }
     }
 }
