acbb2ade86c00e27af04e5c016f0d0946855ba34
paragon0107 <smg0725@naver.com>
2024-10-17 23:47:15 +0900

    DiarySpecificResponse 카테고리 추가

--- src/main/java/org/example/diary/api/DiarySpecificResponse.java
+++ src/main/java/org/example/diary/api/DiarySpecificResponse.java
@@ -1,11 +1,14 @@
 package org.example.diary.api;
 
+import org.example.diary.repository.Category;
+
 import java.time.LocalDateTime;
 
 public record DiarySpecificResponse(
         long id,
         String title,
         String body,
+        Category category,
         LocalDateTime date
         ) {
 }
