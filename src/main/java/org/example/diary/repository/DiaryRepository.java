ab9718b8f8b367c00c53018fc04ef8248047450e
paragon0107 <smg0725@naver.com>
2024-10-21 14:06:28 +0900

    다이어리 조회시 정렬 기준 수정

--- src/main/java/org/example/diary/repository/DiaryRepository.java
+++ src/main/java/org/example/diary/repository/DiaryRepository.java
@@ -1,5 +1,7 @@
 package org.example.diary.repository;
 
+import org.example.diary.repository.entity.Category;
+import org.example.diary.repository.entity.DiaryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Component;
@@ -10,8 +12,8 @@ import java.util.Optional;
 @Component
 public interface DiaryRepository extends JpaRepository<DiaryEntity,Long> {
 
-    @Query("SELECT d FROM DiaryEntity d ORDER BY LENGTH(d.body) DESC")
-    List<DiaryEntity> findAllOrderByBodyLengthDesc();;
+    @Query("SELECT d FROM DiaryEntity d ORDER BY LENGTH(d.body) ASC ")
+    List<DiaryEntity> findAllOrderByBodyLengthDesc();
 
     Optional<DiaryEntity> findByTitle(String title);
 
