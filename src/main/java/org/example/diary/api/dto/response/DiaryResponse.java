c5a97691522837b251da2f23c7ecd242aabf67a3
paragon0107 <smg0725@naver.com>
2024-10-21 14:06:46 +0900

    다이어리 조회시 Body 속성 추가

--- src/main/java/org/example/diary/api/dto/response/DiaryResponse.java
+++ src/main/java/org/example/diary/api/dto/response/DiaryResponse.java
@@ -1,12 +1,14 @@
-package org.example.diary.api;
+package org.example.diary.api.dto.response;
 
 public class DiaryResponse {
     private final long id;
     private final String title;
+    private final String body;
 
-    public DiaryResponse(long id, String title) {
+    public DiaryResponse(long id, String title, String body) {
         this.id = id;
         this.title = title;
+        this.body = body;
     }
 
     public long getId() {
@@ -15,5 +17,9 @@ public class DiaryResponse {
 
     public String getTitle() {
         return title;
+    }
+
+    public String getBody(){
+        return body;
     }
 }
