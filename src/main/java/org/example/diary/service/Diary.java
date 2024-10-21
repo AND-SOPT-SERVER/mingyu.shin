3745c1adadd9edae0c61224fa5fe2cab3a2dde7b
paragon0107 <smg0725@naver.com>
2024-10-21 14:04:22 +0900

    Diary에 Body 추가

--- src/main/java/org/example/diary/service/Diary.java
+++ src/main/java/org/example/diary/service/Diary.java
@@ -3,14 +3,20 @@ package org.example.diary.service;
 public class Diary {
     private final long id;
     private final String title;
+    private final String body;
 
-    public Diary(long id, String title){
+    public Diary(long id, String title, String body){
         this.id = id;
         this.title = title;
+        this.body = body;
     }
 
     public long getId(){
         return id;
+    }
+
+    public String getBody(){
+        return body;
     }
 
     public String getTitle() {
