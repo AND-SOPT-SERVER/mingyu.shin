fac1c8b3d9c215986b9c394e6a027825540cf86c
paragon0107 <smg0725@naver.com>
2024-11-03 23:38:37 +0900

    feat: DiaryFinder 로직 수정

--- src/main/java/org/example/diary/repository/adapter/DiaryFinder.java
+++ src/main/java/org/example/diary/repository/adapter/DiaryFinder.java
@@ -3,7 +3,6 @@ package org.example.diary.repository.adapter;
 import org.example.diary.repository.entity.Category;
 import org.example.diary.repository.entity.DiaryEntity;
 import org.example.diary.repository.DiaryRepository;
-import org.example.diary.service.Diary;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -14,30 +13,19 @@ import java.util.Optional;
 public class DiaryFinder {
     private final DiaryRepository diaryRepository;
 
-    public DiaryFinder (DiaryRepository diaryRepository) {
+    public DiaryFinder(DiaryRepository diaryRepository) {
         this.diaryRepository = diaryRepository;
     }
 
-    public List<Diary> findDiaryList(){
-        final List<DiaryEntity> diaryEntityList = diaryRepository.findAllOrderByBodyLengthDesc();
-        final List<Diary> diaryList = new ArrayList<>();
-        for (DiaryEntity diaryEntity : diaryEntityList) {
-            int diaryLength = diaryList.size();
-            if (
-                    diaryLength < 10 ||
-                            diaryList.get(diaryLength - 1).getBody().length() == diaryEntity.getBody().length()
-            ) {
-                diaryList.add(new Diary(diaryEntity.getId(), diaryEntity.getTitle(), diaryEntity.getBody()));
-            }
-        }
-        return diaryList;
+    public List<DiaryEntity> findDiaryList() {
+        return listPaging(diaryRepository.findAllOrderByBodyLengthAsc());
     }
 
-    public List<DiaryEntity> findAllByCategory(Category category){
-        return diaryRepository.findAllByCategory(category);
+    public List<DiaryEntity> findDiaryListByCategory(final Category category) {
+        return listPaging(diaryRepository.findAllByCategoryOrderByBodyLengthDesc(category));
     }
 
-    public DiaryEntity getDiaryEntity(final long id){
+    public DiaryEntity getDiaryEntity(final long id) {
         Optional<DiaryEntity> diaryEntity = diaryRepository.findById(id);
         if (diaryEntity.isEmpty()) {
             throw new RuntimeException("존재하지 않은 아이디");
@@ -45,7 +33,21 @@ public class DiaryFinder {
         return diaryEntity.get();
     }
 
-    public Optional<DiaryEntity> findByTitle(String title){
+    public Optional<DiaryEntity> findByTitle(final String title) {
         return diaryRepository.findByTitle(title);
+    }
+
+    private List<DiaryEntity> listPaging(final List<DiaryEntity> diaryEntityList) {
+        final List<DiaryEntity> diaryListResponseList = new ArrayList<>();
+        for (DiaryEntity diaryEntity : diaryEntityList) {
+            int diaryLength = diaryListResponseList.size();
+            if (
+                    diaryLength < 10 ||
+                            diaryListResponseList.get(diaryLength - 1).getBody().length() == diaryEntity.getBody().length()
+            ) {
+                diaryListResponseList.add(diaryEntity);
+            }
+        }
+        return diaryEntityList;
     }
 }
