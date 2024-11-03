571907ea9825d159bdfdd79c21840dcdbd779024
paragon0107 <smg0725@naver.com>
2024-11-03 23:35:04 +0900

    feat: 다이어리 컨트롤러 구현

--- src/main/java/org/example/diary/api/DiaryController.java
+++ src/main/java/org/example/diary/api/DiaryController.java
@@ -6,62 +6,63 @@ import org.example.diary.api.dto.request.DiaryUpdateRequest;
 import org.example.diary.api.dto.response.DiaryListResponse;
 import org.example.diary.api.dto.response.DiaryResponse;
 import org.example.diary.api.dto.response.DiarySpecificResponse;
-import org.example.diary.repository.entity.DiaryEntity;
-import org.example.diary.service.Diary;
+import org.example.diary.common.util.validator.diary.DiaryValidator;
 import org.example.diary.service.DiaryService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import static org.example.diary.common.constants.TempValue.TEMP_TITLE;
+
 @RestController
+@RequestMapping("/diaries")
 public class DiaryController {
 
     private final DiaryService diaryService;
+    private final DiaryValidator diaryValidator;
 
-    public DiaryController(DiaryService diaryService) {
+    public DiaryController(DiaryService diaryService, DiaryValidator diaryValidator) {
         this.diaryService = diaryService;
+        this.diaryValidator = diaryValidator;
     }
 
-    @PostMapping("/diaries")
-    void post(@RequestBody DiaryCreateRequest request) {
+    @PostMapping
+    ResponseEntity<Void> post(@RequestBody DiaryCreateRequest request) {
+        diaryValidator.validate(request.title(), request.body());
         diaryService.createDiary(request);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
-    @GetMapping("/diaries")
+    @GetMapping
     ResponseEntity<DiaryListResponse> get() {
-        List<Diary> diaryList = diaryService.getList();
-        List<DiaryResponse> diaryResponseList = new ArrayList<>();
-        for (Diary diary : diaryList) {
-            diaryResponseList.add(new DiaryResponse(diary.getId(), diary.getTitle(),diary.getBody()));
-        }
-        return ResponseEntity.ok(new DiaryListResponse(diaryResponseList));
+        List<DiaryResponse> response = diaryService.getList();
+        return ResponseEntity.ok(new DiaryListResponse(response));
     }
 
-    @GetMapping("/diaries/category")
+    @GetMapping("/category")
     ResponseEntity<DiaryListResponse> getByCategory(@RequestBody final DiaryGetByCategoryRequest request){
-        List<DiaryEntity> diaryList = diaryService.getListByCategory(request.category());
-        List<DiaryResponse> diaryResponseList = new ArrayList<>();
-        for (DiaryEntity diary : diaryList) {
-            diaryResponseList.add(new DiaryResponse(diary.getId(), diary.getTitle(), diary.getBody()));
-        }
-        return ResponseEntity.ok(new DiaryListResponse(diaryResponseList));
+        List<DiaryResponse> diaryList = diaryService.getListByCategory(request.category());
+        return ResponseEntity.ok(new DiaryListResponse(diaryList));
     }
 
-    @GetMapping("/diaries/{id}")
+    @GetMapping("/{id}")
     ResponseEntity<DiarySpecificResponse> getSpecific(@PathVariable final long id) {
         DiarySpecificResponse response =  diaryService.getDiarySpecific(id);
         return ResponseEntity.ok(response);
     }
 
-    @PatchMapping("/diaries/{id}")
-    void updateDiary(@PathVariable final long id,@RequestBody final DiaryUpdateRequest request) {
+    @PatchMapping("/{id}")
+    ResponseEntity<Void> updateDiary(@PathVariable final long id,@RequestBody final DiaryUpdateRequest request) {
+        diaryValidator.validate(TEMP_TITLE.name(), request.body());
         diaryService.updateDiary(id, request);
+        return ResponseEntity.ok().build();
     }
 
-    @DeleteMapping("/diaries/{id}")
-    void deleteDiary(@PathVariable final long id){
+    @DeleteMapping("/{id}")
+    ResponseEntity<Void> deleteDiary(@PathVariable final long id){
         diaryService.deleteDiary(id);
+        return ResponseEntity.ok().build();
     }
 }
