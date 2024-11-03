9bb9bf72d209cd2aa6e865ba87e0f4c5780b7be4
paragon0107 <smg0725@naver.com>
2024-11-03 23:37:31 +0900

    feat: 다이어리 서비스 구현

--- src/main/java/org/example/diary/service/DiaryService.java
+++ src/main/java/org/example/diary/service/DiaryService.java
@@ -1,16 +1,20 @@
 package org.example.diary.service;
 
 import org.example.diary.api.dto.request.DiaryCreateRequest;
+import org.example.diary.api.dto.response.DiaryResponse;
 import org.example.diary.api.dto.response.DiarySpecificResponse;
 import org.example.diary.api.dto.request.DiaryUpdateRequest;
 import org.example.diary.repository.adapter.DiaryDeleter;
 import org.example.diary.repository.adapter.DiaryFinder;
 import org.example.diary.repository.adapter.DiarySaver;
+import org.example.diary.repository.adapter.MemberFinder;
 import org.example.diary.repository.entity.Category;
 import org.example.diary.repository.entity.DiaryEntity;
+import org.example.diary.repository.entity.Member;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -19,20 +23,24 @@ public class DiaryService {
     private final DiaryFinder diaryFinder;
     private final DiarySaver diarySaver;
     private final DiaryDeleter diaryDeleter;
+    private final MemberFinder memberFinder;
 
-    public DiaryService(DiaryFinder diaryFinder,DiarySaver diarySaver,DiaryDeleter diaryDeleter) {
+    public DiaryService(DiaryFinder diaryFinder, DiarySaver diarySaver, DiaryDeleter diaryDeleter, MemberFinder memberFinder) {
         this.diaryFinder = diaryFinder;
         this.diarySaver = diarySaver;
         this.diaryDeleter = diaryDeleter;
+        this.memberFinder = memberFinder;
     }
 
     public void createDiary(final DiaryCreateRequest diaryCreateRequest) {
+        Member member = memberFinder.findMemberById(diaryCreateRequest.userId());
         diarySaver.createDiary(
                 DiaryEntity.of(
                         diaryCreateRequest.title(),
                         diaryCreateRequest.body(),
                         diaryCreateRequest.category(),
-                        LocalDateTime.now()
+                        LocalDateTime.now(),
+                        member
                 ));
     }
 
@@ -43,16 +51,26 @@ public class DiaryService {
                 diaryEntity.getTitle(),
                 diaryEntity.getBody(),
                 diaryEntity.getCategory(),
-                diaryEntity.getDate()
+                diaryEntity.getCreatedAt()
         );
     }
 
-    public List<Diary> getList() {
-        return diaryFinder.findDiaryList();
+    public List<DiaryResponse> getList() {
+        List<DiaryEntity> diaryList = diaryFinder.findDiaryList();
+        final List<DiaryResponse> diaryListResponseList = new ArrayList<>();
+        for (DiaryEntity diaryEntity : diaryList) {
+            diaryListResponseList.add(DiaryResponse.from(diaryEntity));
+        }
+        return diaryListResponseList;
     }
 
-    public List<DiaryEntity> getListByCategory(final Category category) {
-        return diaryFinder.findAllByCategory(category);
+    public List<DiaryResponse> getListByCategory(final Category category) {
+        List<DiaryEntity> diaryListByCategory = diaryFinder.findDiaryListByCategory(category);
+        final List<DiaryResponse> diaryListResponseList = new ArrayList<>();
+        for (DiaryEntity diaryEntity : diaryListByCategory) {
+            diaryListResponseList.add(DiaryResponse.from(diaryEntity));
+        }
+        return diaryListResponseList;
     }
 
     public void updateDiary(final long id, final DiaryUpdateRequest request) {
