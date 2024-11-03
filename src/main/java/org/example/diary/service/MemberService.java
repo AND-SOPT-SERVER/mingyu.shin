b24c2ff5e9fa8cdd299ce24da6ca563943e79e79
paragon0107 <smg0725@naver.com>
2024-11-03 23:31:50 +0900

    feat: 멤버 서비스 구현

---
+++ src/main/java/org/example/diary/service/MemberService.java
@@ -0,0 +1,33 @@
+package org.example.diary.service;
+
+import org.example.diary.api.dto.request.CreateMemberRequest;
+import org.example.diary.api.dto.request.SignInRequest;
+import org.example.diary.repository.adapter.MemberFinder;
+import org.example.diary.repository.adapter.MemberSaver;
+import org.example.diary.repository.entity.Member;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MemberService {
+
+    private final MemberFinder memberFinder;
+    private final MemberSaver memberSaver;
+
+
+    public MemberService(MemberFinder memberFinder, MemberSaver memberSaver) {
+        this.memberFinder = memberFinder;
+        this.memberSaver = memberSaver;
+    }
+
+    public void createMember(CreateMemberRequest request){
+        memberSaver.createMember(new Member(request.userName(), request.password(), request.nickname()));
+    }
+
+    public long signIn(SignInRequest request){
+        Member member = memberFinder.findUserByUserName(request.username());
+        if (!member.getPassword().equals(request.password())){
+            throw new RuntimeException("비밀번호가 틀렸습니다.");
+        }
+        return member.getId();
+    }
+}
